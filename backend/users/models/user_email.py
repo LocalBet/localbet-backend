9b1e5b0e91df2356325f9d123f076d4711b4cd23
Guillem Pozo S. <58363308+guillem-ps@users.noreply.github.com>
2025-11-08 22:19:30 +0100

    refactor: Remove commented-out validation method for user name in UserEmail class

--- backend/users/models/user_email.py
+++ backend/users/models/user_email.py
@@ -39,32 +39,3 @@ class UserEmail(ValueObject[str]):
         if len(value) > self.__USER_EMAIL_MAX_LENGTH:
             raise UserEmailTypeError(email=value)
 
-    # @override
-    # def _validate(self, value: str) -> None:
-    #     """
-    #     This method validates that the value follows the domain rules.
-
-    #     Args:
-    #         value (str): User name.
-
-    #     Raises:
-    #         UserNameTypeError: If the value is not a string.
-    #         UserNameMinLengthError: If the value length is less than the minimum allowed.
-    #         UserNameMaxLengthError: If the value length is greater than the maximum allowed.
-    #         UserNameContainsInvalidCharactersError: If the value contains invalid characters.
-    #         UserNameContainsInvalidCharactersError: If the value contains leading or trailing whitespaces.
-    #     """
-    #     if type(value) is not str:
-    #         raise UserNameTypeError(name=value)
-
-    #     if len(value) < self.__USER_NAME_MIN_LENGTH:
-    #         raise UserNameMinLengthError(name=value, min_length=self.__USER_NAME_MIN_LENGTH)
-
-    #     if len(value) > self.__USER_NAME_MAX_LENGTH:
-    #         raise UserNameMaxLengthError(name=value, max_length=self.__USER_NAME_MAX_LENGTH)
-
-    #     if not value.isprintable():
-    #         raise UserNameContainsInvalidCharactersError(name=value)
-
-    #     if value != value.strip():
-    #         raise UserNameContainsInvalidCharactersError(name=value)
