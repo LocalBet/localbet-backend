fb902b86ff350c80b428f9fda1bc37fb869f374c
ZerreMC <zerreytug.gll@gmail.com>
2025-12-17 19:52:27 +0100

    Fix: update wallettransaction

--- backend/users/models/__init__.py
+++ backend/users/models/__init__.py
@@ -3,5 +3,6 @@ from .user_coins import UserCoins
 from .user_email import UserEmail
 from .user_password import UserPassword
 from .user_username import UserUsername
+from .wallet_transaction import WalletTransaction
 
-__all__ = ["User", "UserEmail", "UserPassword", "UserUsername", "UserCoins"]
+__all__ = ["User", "UserEmail", "UserPassword", "UserUsername", "UserCoins", "WalletTransaction"]
